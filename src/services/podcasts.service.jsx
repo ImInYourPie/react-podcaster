a89ce0067699ab7af6b52174275521f5010321ac
ImInYourPie <36893864+ImInYourPie@users.noreply.github.com>
2023-02-28 10:18:06 +0000

    getPodcast service

--- src/services/podcasts.service.jsx
+++ src/services/podcasts.service.jsx
@@ -1,10 +1,19 @@
-const URL = (limit = 100) =>
-  `https://itunes.apple.com/us/rss/toppodcasts/limit=${limit}/genre=1310/json`;
-
 const makePodcastsService = ({ request }) => ({
   list: async (limit) => {
     try {
-      const data = await request.get(URL(limit));
+      const data = await request.get(
+        `https://itunes.apple.com/us/rss/toppodcasts/limit=${limit}/genre=1310/json`
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getPodcast: async (id) => {
+    try {
+      const data = await request.get(
+        `https://itunes.apple.com/lookup?id=${id}&entity=podcastEpisode`
+      );
       return data;
     } catch (error) {
       console.log(error);
