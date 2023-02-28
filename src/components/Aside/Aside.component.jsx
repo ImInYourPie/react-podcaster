6cdab2105efd81f92a6f60389e2384aeea8dc7a0
ImInYourPie <36893864+ImInYourPie@users.noreply.github.com>
2023-02-28 11:04:04 +0000

    Remove action area from aside component

--- src/components/Aside/Aside.component.jsx
+++ src/components/Aside/Aside.component.jsx
@@ -1,33 +1,25 @@
 import React from "react";
 
 // MUI
-import {
-  Card,
-  CardContent,
-  CardActionArea,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const Aside = () => {
   return (
     <Card>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={""}
-          alt={`Cover image for ${""} podcast`}
-          aria-label={`Cover image for ${""} podcast`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="subtitle1">
-            test
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-            test
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        component="img"
+        image={""}
+        alt={`Cover image for ${""} podcast`}
+        aria-label={`Cover image for ${""} podcast`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="subtitle1">
+          test
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          test
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
