d9e40efe582e9ed5a4ea7d89e7b29be1d624b355
ImInYourPie <36893864+ImInYourPie@users.noreply.github.com>
2023-03-01 11:44:07 +0000

    Rework episodes list: use table

--- src/features/podcast/components/EpisodeItem.component.jsx
+++ src/features/podcast/components/EpisodeItem.component.jsx
@@ -1,3 +1,5 @@
+// @Deprecated
+
 import React from "react";
 import { Link } from "react-router-dom";
 
--- src/features/podcast/components/EpisodesList.component.jsx
+++ src/features/podcast/components/EpisodesList.component.jsx
@@ -1,29 +1,57 @@
 import React from "react";
-
-// Components
-import EpisodeItem from "./EpisodeItem.component";
-import SubHeader from "./SubHeader.component";
+import { Link } from "react-router-dom";
 
 // Hooks
 import { usePodcast } from "..";
 
 // MUI
-import { Card, Box, CardContent, CardHeader } from "@mui/material";
+import {
+  Card,
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+} from "@mui/material";
 
 const EpisodesList = () => {
-  const { episodes } = usePodcast();
+  const { episodes, podcast } = usePodcast();
 
   return (
     <Box mx={2}>
-      <Card>
-        <CardContent>
-          <SubHeader />
-
-          {episodes.map((episode) => (
-            <EpisodeItem key={episode.id} episode={episode} />
-          ))}
-        </CardContent>
-      </Card>
+      <TableContainer component={Card}>
+        <Table aria-label={`Table of episodes from "${podcast.title}" podcast`}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Date</TableCell>
+              <TableCell align="right">Duration</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {episodes.map((episode) => (
+              <TableRow
+                key={episode.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={`episode/${episode.id}`}
+                  >
+                    <Typography>{episode.title}</Typography>
+                  </Link>
+                </TableCell>
+                <TableCell align="right">{episode.date}</TableCell>
+                <TableCell align="right">{episode.duration}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
